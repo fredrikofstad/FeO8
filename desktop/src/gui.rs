5cd88d65e8800f818f8f8c093d029e72ff182ce8
fredrikofstad <nokotin@gmail.com>
2024-09-24 21:28:26 +0900

    User input

--- desktop/src/gui.rs
+++ desktop/src/gui.rs
@@ -6,13 +6,15 @@ use sdl2::pixels::Color;
 use sdl2::rect::Rect;
 use sdl2::render::Canvas;
 use sdl2::video::Window;
+use sdl2::keyboard::Keycode;
 
+const TICKS_PER_FRAME: usize = 10;
 
 const SCALE: u32 = 15;
 const WIDTH: u32 = (feo_core::SCREEN_WIDTH as u32) * SCALE;
 const HEIGHT: u32 = (feo_core::SCREEN_HEIGHT as u32) * SCALE;
 
-pub fn init(mut emu: &mut Emulation) {
+pub fn init(emu: &mut Emulation) {
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
     let window = video_subsystem
@@ -30,13 +32,28 @@ pub fn init(mut emu: &mut Emulation) {
     'game_loop: loop {
         for evt in event_pump.poll_iter() {
             match evt {
-                Event::Quit{..} => {
+                Event::Quit{..} | Event::KeyDown{keycode: Some(Keycode::Escape), ..}=> {
                     break 'game_loop;
                 },
+
+                Event::KeyDown{keycode: Some(key), ..} => {
+                    if let Some(k) = key_input(key) {
+                        emu.key_press(k, true);
+                    }
+                },
+                Event::KeyUp{keycode: Some(key), ..} => {
+                    if let Some(k) = key_input(key) {
+                        emu.key_press(k, false);
+                    }
+                },
+
                 _ => ()
             }
         }
-        emu.tick();
+        for _ in 0..TICKS_PER_FRAME {
+            emu.tick();
+        }
+        emu.tick_timers();
         draw_screen(emu, &mut canvas);
     }
 }
@@ -60,3 +77,26 @@ fn draw_screen(emu: &Emulation, canvas: &mut Canvas<Window>) {
     }
     canvas.present();
 }
+
+fn key_input(key: Keycode) -> Option<usize> {
+    match key {
+        Keycode::Num1 => Some(0x1),
+        Keycode::Num2 => Some(0x2),
+        Keycode::Num3 => Some(0x3),
+        Keycode::Num4 => Some(0xC),
+        Keycode::Q => Some(0x4),
+        Keycode::W => Some(0x5),
+        Keycode::E => Some(0x6),
+        Keycode::R => Some(0xD),
+        Keycode::A => Some(0x7),
+        Keycode::S => Some(0x8),
+        Keycode::D => Some(0x9),
+        Keycode::F => Some(0xE),
+        Keycode::Z => Some(0xA),
+        Keycode::X => Some(0x0),
+        Keycode::C => Some(0xB),
+        Keycode::V => Some(0xF),
+        _ => None,
+    }
+}
+
